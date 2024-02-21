a7b4d6d162ee23991b9e4cb97da1c8e96058e504
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-22 00:16:01 +0530

    feat(footer): footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import { HeroSection } from '@/components/HeroSection';
 import { InfoSection } from '@/components/InfoSection';
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <HeroSection />
       <InfoSection />
+      <Footer />
     </>
   );
 }
