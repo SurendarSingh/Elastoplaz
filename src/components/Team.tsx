76f9d4c14732ac5f97436313cf359b40bedc8cb4
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-23 00:42:12 +0530

    feat(team): team photos added

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,57 +7,49 @@ const people = [
     id: 1,
     name: 'Selva Nithilan',
     designation: 'Chairperson',
-    image:
-      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
+    image: '/Team/selva.jpg',
   },
   {
     id: 2,
     name: 'Santhosh Sakthi',
     designation: 'Vice-Chairperson',
-    image:
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+    image: '/Team/sakthi.jpg',
   },
   {
     id: 3,
     name: 'Vishnu Gopal',
     designation: 'Treasurer',
-    image:
-      'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+    image: '/Team/vishnu.jpg',
   },
   {
     id: 4,
     name: 'Dhanamukhi',
     designation: 'Treasurer',
-    image:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    image: '/Team/dhana.jpg',
   },
   {
     id: 5,
     name: 'Sivanesh',
     designation: 'Program Secretary',
-    image:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
+    image: '/Team/sivanesh.jpg',
   },
   {
     id: 6,
     name: 'Gladis Christina Jaya',
     designation: 'Program Secretary',
-    image:
-      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
+    image: '/Team/gladish.jpg',
   },
   {
     id: 7,
     name: 'Divyadharshini',
     designation: 'Event Coordinator',
-    image:
-      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
+    image: '/Team/divya.jpg',
   },
   {
     id: 8,
     name: 'Sahaana',
     designation: 'Placement Represantative',
-    image:
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+    image: '/Team/sahanna.jpg',
   },
   {
     id: 9,
@@ -103,6 +95,12 @@ const people = [
   },
   {
     id: 15,
+    name: 'Angeline',
+    designation: 'IRI Coordinator ',
+    image: '/Team/angeline.jpg',
+  },
+  {
+    id: 16,
     name: 'Naresh',
     designation: 'Library Incharge',
     image:
@@ -119,7 +117,7 @@ export function Team() {
       <div className='mb-2 mt-8 flex w-full flex-row items-center justify-center'>
         <AnimatedTooltip items={people} />
       </div>
-      <p className='inter-var text-center text-base font-normal text-white md:text-lg'>
+      <p className='inter-var text-center text-lg font-semibold text-white md:text-lg'>
         SPARTAN &apos;24
       </p>
     </WavyBackground>
