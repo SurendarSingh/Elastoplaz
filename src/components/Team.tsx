1a19b862a5bb0e955555cad7de4b5306ecfcfb42
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-22 16:54:03 +0530

    chore(team): team list updated

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { WavyBackground } from './WavyBackground';
 import { AnimatedTooltip } from './AnimatedToolTip';
@@ -8,42 +6,105 @@ const people = [
   {
     id: 1,
     name: 'Selva Nithilan',
-    designation: 'Chairman',
+    designation: 'Chairperson',
     image:
       'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
   },
   {
     id: 2,
     name: 'Santhosh Sakthi',
-    designation: 'Vice Chairman',
+    designation: 'Vice-Chairperson',
     image:
       'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
   },
   {
     id: 3,
-    name: 'Jane Smith',
-    designation: 'Data Scientist',
+    name: 'Vishnu Gopal',
+    designation: 'Treasurer',
     image:
       'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
   },
   {
     id: 4,
-    name: 'Emily Davis',
-    designation: 'UX Designer',
+    name: 'Dhanamukhi',
+    designation: 'Treasurer',
     image:
       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
   },
   {
     id: 5,
-    name: 'Tyler Durden',
-    designation: 'Soap Developer',
+    name: 'Sivanesh',
+    designation: 'Program Secretary',
     image:
       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
   },
   {
     id: 6,
-    name: 'Dora',
-    designation: 'The Explorer',
+    name: 'Gladis Christina Jaya',
+    designation: 'Program Secretary',
+    image:
+      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
+  },
+  {
+    id: 7,
+    name: 'Event Coordinator',
+    designation: 'Divyadharshini',
+    image:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
+  },
+  {
+    id: 8,
+    name: 'Sahaana',
+    designation: 'Placement Represantative',
+    image:
+      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    id: 9,
+    name: 'Goutham',
+    designation: 'Spart Represantative',
+    image:
+      'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    id: 10,
+    name: 'Srinivasan',
+    designation: 'Alumni Coordinator',
+    image:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+  },
+  {
+    id: 11,
+    name: 'Ahilandeswari',
+    designation: 'Alumni Coordinator',
+    image:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
+  },
+  {
+    id: 12,
+    name: 'Salaisiva',
+    designation: 'Alumni Coordinator',
+    image:
+      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
+  },
+  {
+    id: 13,
+    name: 'Sangeetha',
+    designation: 'Alumni Coordinator',
+    image:
+      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
+  },
+  {
+    id: 14,
+    name: 'Gnaneshwaran',
+    designation: 'IRI Coordinator ',
+    image:
+      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
+  },
+  {
+    id: 15,
+    name: 'Naresh',
+    designation: 'Library Incharge',
     image:
       'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80',
   },
@@ -52,7 +113,7 @@ const people = [
 export function Team() {
   return (
     <WavyBackground className='mx-auto max-w-4xl py-40'>
-      <p className='inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl'>
+      <p className='inter-var text-center text-3xl font-bold text-white md:text-4xl lg:text-7xl'>
         Meet our Cool Team
       </p>
       <div className='mb-2 mt-8 flex w-full flex-row items-center justify-center'>
