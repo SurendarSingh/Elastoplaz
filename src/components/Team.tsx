d2c82d27ca0439a871066ba8df1189942911a31e
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-22 16:22:43 +0530

    fix(team): text updated

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,15 +7,15 @@ import { AnimatedTooltip } from './AnimatedToolTip';
 const people = [
   {
     id: 1,
-    name: 'John Doe',
-    designation: 'Software Engineer',
+    name: 'Selva Nithilan',
+    designation: 'Chairman',
     image:
       'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80',
   },
   {
     id: 2,
-    name: 'Robert Johnson',
-    designation: 'Product Manager',
+    name: 'Santhosh Sakthi',
+    designation: 'Vice Chairman',
     image:
       'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
   },
@@ -59,7 +59,7 @@ export function Team() {
         <AnimatedTooltip items={people} />
       </div>
       <p className='inter-var text-center text-base font-normal text-white md:text-lg'>
-        Spartian 2024
+        SPARTAN &apos;24
       </p>
     </WavyBackground>
   );
