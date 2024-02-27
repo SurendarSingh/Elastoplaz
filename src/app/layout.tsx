d816fafdb60308988c3276336f80f36392472ad8
Surendar Singh <surendarsingh2002@gmail.com>
2024-02-27 19:20:44 +0530

    feat(speed-insights): vercel speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
